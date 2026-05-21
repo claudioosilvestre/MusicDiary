2130274ff1cad044a8c406b86e561d0c7bc353b0
Cláudio Silvestre <claudioosilvestre@hotmail.com>
2026-05-21 14:35:40 +0000

    add JwtService with token generation and extraction

---
+++ backend/src/main/java/com/musicdiary/exceptions/EmailNotFoundException.java
@@ -0,0 +1,7 @@
+package com.musicdiary.exceptions;
+
+public class EmailNotFoundException extends MusicDiaryException {
+    public EmailNotFoundException() {
+        super("Email not found");
+    }
+}
---
+++ backend/src/main/java/com/musicdiary/services/JwtServiceImpl.java
@@ -0,0 +1,60 @@
+package com.musicdiary.services;
+
+import com.musicdiary.exceptions.EmailNotFoundException;
+import com.musicdiary.models.User;
+import com.musicdiary.repositories.UserRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Service
+public class JwtServiceImpl implements JwtService {
+
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    @Value("${jwt.expiration}")
+    private long expiration;
+
+    @Override
+    public String generateToken(User user) {
+
+        SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+        Date now = new Date();
+        Date expiryDate = new Date(now.getTime() + expiration);
+
+        return Jwts.builder()
+                .subject(user.getEmail())
+                .issuedAt(now)
+                .expiration(expiryDate)
+                .claim("project", "MusicDiary")
+                .signWith(key)
+                .compact();
+    }
+
+    @Override
+    public String extractEmail(String token) {
+        try{
+            SecretKey key = Keys.hmacShaKeyFor(secret.getBytes(StandardCharsets.UTF_8));
+
+            Claims claims = Jwts.parser()
+                    .verifyWith(key)
+                    .build()
+                    .parseSignedClaims(token)
+                    .getPayload();
+
+            return claims.getSubject();
+        } catch (JwtException | IllegalArgumentException exception) {
+            return null;
+        }
+    }
+}
