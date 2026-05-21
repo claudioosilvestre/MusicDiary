14b0bc452760bab126d36cd377832d14cf937f38
Cláudio Silvestre <claudioosilvestre@hotmail.com>
2026-05-21 14:02:07 +0000

    add securityconfig, exceptions, and part of the AuthServiceImpl

--- backend/src/main/java/com/musicdiary/services/AuthService.java
+++ backend/src/main/java/com/musicdiary/services/AuthService.java
@@ -1,4 +1,9 @@
 package com.musicdiary.services;
 
+import com.musicdiary.dtos.RegisterRequestDTO;
+
+
 public interface AuthService {
+
+    String register(RegisterRequestDTO registerRequestDTO);
 }
--- backend/src/main/java/com/musicdiary/services/AuthServiceImpl.java
+++ backend/src/main/java/com/musicdiary/services/AuthServiceImpl.java
@@ -1,4 +1,43 @@
 package com.musicdiary.services;
 
-public class AuthServiceImpl {
+import com.musicdiary.dtos.RegisterRequestDTO;
+import com.musicdiary.exceptions.UserEmailAlreadyExistsException;
+import com.musicdiary.models.User;
+import com.musicdiary.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    private UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Override
+    public String register(RegisterRequestDTO registerRequestDTO) {
+        if(userRepository.findByEmail(registerRequestDTO.getEmail()).isPresent()) {
+            throw new UserEmailAlreadyExistsException();
+        }
+
+        String password = passwordEncoder.encode(registerRequestDTO.getPassword());
+
+        User user = new User();
+        user.setFirstName(registerRequestDTO.getFirstName());
+        user.setLastName(registerRequestDTO.getLastName());
+        user.setBirthDate(registerRequestDTO.getBirthDate());
+        user.setEmail(registerRequestDTO.getEmail());
+        user.setPasswordHash(password);
+
+        userRepository.save(user);
+
+        return null;
+    }
+
+
 }
