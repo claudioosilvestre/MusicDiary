f4660b1bdb5ff9690c21aa2cdb21f1e902b0368d
Cláudio Silvestre <claudioosilvestre@hotmail.com>
2026-05-21 09:52:38 +0100

    Add files via upload

---
+++ backend/src/main/java/com/musicdiary/MusicdiaryApplication.java
@@ -0,0 +1,13 @@
+package com.musicdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicdiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicdiaryApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/musicdiary/MusicdiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musicdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicdiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
