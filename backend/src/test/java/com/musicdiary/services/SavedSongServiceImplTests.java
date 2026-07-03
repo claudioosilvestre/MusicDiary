cec979d009cea2a968d0b27b36ddb7c4863dec37
Cláudio Silvestre <claudioosilvestre@hotmail.com>
2026-07-03 11:07:03 +0100

    add tests to savedSongServiceImplTests

--- backend/src/test/java/com/musicdiary/services/SavedSongServiceImplTests.java
+++ backend/src/test/java/com/musicdiary/services/SavedSongServiceImplTests.java
@@ -3,7 +3,10 @@ package com.musicdiary.services;
 import com.musicdiary.dtos.EditNoteRequestDTO;
 import com.musicdiary.dtos.SaveSongRequestDTO;
 import com.musicdiary.dtos.SaveSongResponseDTO;
+import com.musicdiary.dtos.SavedSongFilterRequestDTO;
 import com.musicdiary.exceptions.SavedSongNotFoundException;
+import com.musicdiary.exceptions.SongAlreadySavedException;
+import com.musicdiary.exceptions.UserNotFoundException;
 import com.musicdiary.models.SavedSong;
 import com.musicdiary.models.Song;
 import com.musicdiary.models.User;
@@ -114,6 +117,75 @@ public class SavedSongServiceImplTests {
     }
 
     @Test
+    void listByArtist_shouldReturnListOfSavedSongResponseDTO() {
+
+        Authentication authentication = mock(Authentication.class);
+        SecurityContext securityContext = mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getName()).thenReturn("test@email.com");
+        SecurityContextHolder.setContext(securityContext);
+
+        User user = new User();
+        user.setId(1L);
+        user.setEmail("test@email.com");
+
+        when(userRepository.findByEmail("test@email.com")).thenReturn(Optional.of(user));
+
+        Song song = new Song();
+        song.setId(1L);
+        song.setTitle("test");
+        song.setArtistName("testName");
+
+        SavedSong savedSong = new SavedSong();
+        savedSong.setId(1L);
+        savedSong.setUser(user);
+        savedSong.setSong(song);
+
+        List<SavedSong> savedSongList = new ArrayList<>();
+        savedSongList.add(savedSong);
+
+        when(savedSongRepository.findByUserAndSongArtistName(user, "testName")).thenReturn(savedSongList);
+
+        List<SaveSongResponseDTO> result = savedSongServiceImpl.listByArtist("testName");
+
+        assertEquals(1, result.size());
+        assertEquals("testName",result.get(0).getArtistName());
+        assertEquals("test",result.get(0).getTitle());
+        verify(savedSongRepository).findByUserAndSongArtistName(user, "testName");
+    }
+
+    @Test
+    void getSavedSongs_shouldThrowExceptionIfSaveSongFilterRequestDTOIsNull() {
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> savedSongServiceImpl.getSavedSongs(null));
+
+        assertEquals("saveSongFilterRequestDTO must be valid", exception.getMessage());
+    }
+
+    @Test
+    void getSavedSongs_shouldThrowExceptionIfUserNotFound() {
+
+        Authentication authentication = mock(Authentication.class);
+        SecurityContext securityContext = mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getName()).thenReturn("test@email.com");
+        SecurityContextHolder.setContext(securityContext);
+
+        SavedSongFilterRequestDTO savedSongFilterRequestDTO = new SavedSongFilterRequestDTO();
+
+        when(userRepository.findByEmail("test@email.com")).thenReturn(Optional.empty());
+
+        UserNotFoundException exception = assertThrows(
+                UserNotFoundException.class,
+                () -> savedSongServiceImpl.getSavedSongs(savedSongFilterRequestDTO));
+
+        assertEquals("User not found", exception.getMessage());
+        verify(userRepository, times(1)).findByEmail("test@email.com");
+    }
+
+    @Test
     void saveSong_withValidData_shouldReturnSavedSong() {
 
         Authentication authentication = mock(Authentication.class);
@@ -146,6 +218,37 @@ public class SavedSongServiceImplTests {
         assertNotNull(songResponseDTO);
         assertEquals("Test", songResponseDTO.getTitle());
         assertEquals("TestArtist", songResponseDTO.getArtistName());
+    }
+
+    @Test
+    void saveSong_shouldThrowExceptionIfSaveSongRequestDTOIsNull() {
+
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> savedSongServiceImpl.saveSong(null));
+
+        assertEquals("SaveSongRequest must be valid", exception.getMessage());
+    }
+
+    @Test
+    void saveSong_shouldThrowExceptionIfUserNotFound() {
+
+        Authentication authentication = mock(Authentication.class);
+        SecurityContext securityContext = mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getName()).thenReturn("test@email.com");
+        SecurityContextHolder.setContext(securityContext);
+
+        SaveSongRequestDTO saveSongRequestDTO = new SaveSongRequestDTO();
+
+        when(userRepository.findByEmail("test@email.com")).thenReturn(Optional.empty());
+
+        UsernameNotFoundException exception = assertThrows(
+                UsernameNotFoundException.class,
+                () -> savedSongServiceImpl.saveSong(saveSongRequestDTO));
+
+        assertEquals("User not found", exception.getMessage());
+        verify(userRepository, times(1)).findByEmail("test@email.com");
     }
 
     @Test
@@ -184,6 +287,39 @@ public class SavedSongServiceImplTests {
         assertNotNull(songResponseDTO);
         assertEquals("test", songResponseDTO.getTitle());
         assertEquals("testArtist", songResponseDTO.getArtistName());
+    }
+
+    @Test
+    void saveSong_shouldThrowExceptionIfSongAlreadySavedByUser() {
+
+        Authentication authentication = mock(Authentication.class);
+        SecurityContext securityContext = mock(SecurityContext.class);
+        when(securityContext.getAuthentication()).thenReturn(authentication);
+        when(authentication.getName()).thenReturn("test@email.com");
+        SecurityContextHolder.setContext(securityContext);
+
+        User user = new User();
+        user.setFirstName("test");
+        user.setEmail("test@email.com");
+
+        SaveSongRequestDTO saveSongRequestDTO = new SaveSongRequestDTO();
+        saveSongRequestDTO.setArtistName("test");
+        saveSongRequestDTO.setTitle("testTitle");
+
+        Song song = new Song();
+        song.setArtistName("test");
+        song.setTitle("testTitle");
+
+        when(userRepository.findByEmail("test@email.com")).thenReturn(Optional.of(user));
+        when(songRepository.findByTitleAndArtistName("testTitle", "test")).thenReturn(Optional.of(song));
+        when(savedSongRepository.existsByUserAndSong(user, song)).thenReturn(true);
+
+        SongAlreadySavedException exception = assertThrows(
+                SongAlreadySavedException.class,
+                () -> savedSongServiceImpl.saveSong(saveSongRequestDTO));
+
+        assertEquals("This song is already saved", exception.getMessage());
+        verify(savedSongRepository).existsByUserAndSong(user, song);
     }
 
     @Test
